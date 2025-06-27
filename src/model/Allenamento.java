8fe11687fc39d9f1a3e6c1ce88035ccda297b2a8
Giuseppe Ravesi <PC@MSI>
2025-06-27 11:13:43 +0200

    Aggiunto Allenamento e Amichevole (senza lista disponibilità)

---
+++ src/model/Allenamento.java
@@ -0,0 +1,49 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Allenamento extends Evento {
+
+    private String tipologia; 
+    private String note;      
+
+    public Allenamento(LocalDate data, LocalTime orario, LocalTime durata, String luogo, String tipologia, String note) {
+        super(data, orario, durata, luogo);
+        this.tipologia = tipologia;
+        this.note = note;
+    }
+
+    //per caricamento da file
+    public Allenamento(int id, LocalDate data, LocalTime orario, LocalTime durata, String luogo, String tipologia, String note) {
+        super(id, data, orario, durata, luogo);
+        this.tipologia = tipologia;
+        this.note = note;
+    }
+
+    public String getTipologia() {
+        return tipologia;
+    }
+
+    public void setTipologia(String tipologia) {
+        this.tipologia = tipologia;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    @Override
+    public String getTipoEvento() {
+        return "Allenamento";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Tipologia: " + tipologia + ", Note: " + note;
+    }
+}
---
+++ src/model/Amichevole.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Amichevole extends Evento {
+    private String squadraAvversaria;
+
+    public Amichevole(LocalDate data, LocalTime orario, LocalTime durata, String luogo, String squadraAvversaria) {
+        super(data, orario, durata, luogo);
+        this.squadraAvversaria = squadraAvversaria;
+    }
+
+    public Amichevole(int id, LocalDate data, LocalTime orario, LocalTime durata, String luogo, String squadraAvversaria) {
+        super(id, data, orario, durata, luogo);
+        this.squadraAvversaria = squadraAvversaria;
+    }
+
+    public String getSquadraAvversaria() {
+        return squadraAvversaria;
+    }
+
+    public void setSquadraAvversaria(String squadraAvversaria) {
+        this.squadraAvversaria = squadraAvversaria;
+    }
+
+    @Override
+    public String getTipoEvento() {
+        return "Amichevole";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Avversario: " + squadraAvversaria;
+    }
+}
+
