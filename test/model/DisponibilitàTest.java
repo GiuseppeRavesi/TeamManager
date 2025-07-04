c29457e27470c1be5accbfff7fd98a9fa555f2e6
vinzven <vnzvcn02h05b428t@studium.unict.it>
2025-07-04 16:30:42 +0200

    Test JUnit4 su Disponibilità;testata "presenza" costruttore, OK

---
+++ test/model/DisponibilitàTest.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package model;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author enzov
+ */
+public class DisponibilitàTest {
+    
+    private Disponibilità d1;
+    private Disponibilità d2;
+    private String m1;
+    
+    @Before
+    public void setUp() {
+        
+        m1 = "Frattura al femore";
+        
+        d1 = new Disponibilità(1, 1, true, null);
+        d2 = new Disponibilità(1, 2, false, m1);
+    }
+    
+    @Test
+    public void testMetodoCostruttore(){
+        //verifico se la creazione di disponilità senza motivazione va a buon fine
+        assertNull(d1.getMotivazione());
+        assertEquals(m1,d2.getMotivazione());
+        
+    }
+
+  
+}
